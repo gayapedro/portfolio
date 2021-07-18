0ca2eb15497b1649ecd0661748ff8dc95596ec2e
gayapedro <pedrogaya@gmail.com>
2021-07-18 17:47:14 -0300

    about me section

---
+++ src/components/About/index.js
@@ -0,0 +1,33 @@
+import './style.css';
+import profilePic from '../../assets/profilepic.JPG';
+
+function About() {
+  return (
+    <div className='about-container'>
+      <h1>Sobre mim</h1>
+      <div className='about-info'>
+        <img src={profilePic} alt='foto de perfil' />
+        <div className='about-texts'>
+          <p>
+            Natural de São Paulo - SP, moro em Salvador - BA desde os 2 anos. Um
+            amante da leitura, apaixonado pelo universo de Harry Potter desde
+            criança. Tenho uma pet chamada Arya (o que entrega meu vício em
+            séries).
+          </p>
+          <p>
+            Sempre curioso, estou em constante busca de novos conhecimentos.
+            Além de programação, me interesso muito em aprender mais sobre
+            culinária e astrofísica.
+          </p>
+          <p>
+            Possuo experiência no desenvolvimento de aplicações front-end
+            utilizando HTML5, CSS3 e ReactJS e no desenvolvimento de aplicações
+            back-end utilizando NodeJS, express e postgresql.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
