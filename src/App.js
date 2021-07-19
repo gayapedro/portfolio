3af71359dc6550240cdbc1726a139a7f7c95ab2b
gayapedro <pedrogaya@gmail.com>
2021-07-19 15:36:23 -0300

    scroll to top

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from './components/Header';
 import About from './components/About';
 import Education from './components/Education';
 import SideBar from './components/SideBar';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div className='App'>
       <SideBar />
+      <ScrollToTop />
       <Header />
       <About />
       <Education />
