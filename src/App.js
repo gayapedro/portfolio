eedb55795db8af92bbb246323ac7a37b96de0129
Pedro Gaya <pedrogaya@gmail.com>
2021-07-22 15:01:57 -0300

    test netlify form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Education from './components/Education';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <About />
       <Education />
       <Projects />
+      <Contact />
     </div>
   );
 }
