2a5a301f45f937d26e59e3be7329cb0ea0087a38
Pedro Gaya <pedrogaya@gmail.com>
2021-07-22 15:12:01 -0300

    test netlify form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,62 @@
+import { useState } from 'react';
+
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&');
+  };
+
+  const handleSubmit = (e) => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => alert('Success!'))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+  };
+
   return (
-    <form name='contact' method='post'>
-      <input type='hidden' name='form-name' value='contact' />
+    <form onSubmit={(e) => handleSubmit(e)}>
       <p>
         <label>
-          Your Name: <input type='text' name='name' />
+          Your Name:{' '}
+          <input
+            type='text'
+            name='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
       </p>
       <p>
         <label>
-          Your Email: <input type='email' name='email' />
+          Your Email:{' '}
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
       </p>
       <p>
         <label>
-          Message: <textarea name='message'></textarea>
+          Message:{' '}
+          <textarea
+            name='message'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </label>
       </p>
       <p>
